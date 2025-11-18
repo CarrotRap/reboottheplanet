77b6d2fef34632c1de60cabad48e09d52bd30c35
Boutonnet Valentin <boutonnet.valentin@student-cs.fr>
2025-11-18 13:58:37 +0100

    changes 7

--- api/join.js
+++ api/join.js
@@ -1,30 +1,21 @@
 import { parse } from "querystring";
-import { sql } from "@vercel/postgres";
 
-export default function handler(req, res) {
-    if (req.method !== "POST") {
-        return res.status(405).json({ error: "Méthode non autorisée" });
+const datas = [];
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Méthode non autorisée' });
     }
 
-    let raw = "";
-
-    req.on("data", chunk => raw += chunk);
-
-    req.on("end", async () => {
-        const data = parse(raw);   // { email: "...", name: "...", etc }
-
-        try {
-            // Insert dans la base
-            await sql`
-        INSERT INTO joins (json)
-        VALUES (${JSON.stringify(data)})
-      `;
-
-            return res.status(200).json({ ok: true });
-
-        } catch (err) {
-            console.error("DB ERROR:", err);
-            return res.status(500).json({ error: "Erreur serveur" });
-        }
+    let body = '';
+    req.on('data', chunk => (body += chunk));
+    req.on('end', () => {
+        const data = parse(body); // Form data
+        console.log(data);
+        datas.push(data);
     });
+
+    return res.status(200).json({ message: 'Formulaire bien reçu !' });
 }
+
+setInterval(() => console.log(datas), 60000)
